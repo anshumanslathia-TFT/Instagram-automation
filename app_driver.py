81d7bb5ad5e9f1d8781b8a7d98a534d21e0779a1
Rishabh Gupta <rishabh.gupta@promo.com>
2022-11-02 13:32:01 +0530

    language selection done

--- app_driver.py
+++ app_driver.py
@@ -1,46 +1,37 @@
 import os
 import pdb
-
 from appium import webdriver
-from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 import time
 
-# options = UiAutomator2Options().load_capabilities({
-#     # Set URL of the application under test
-#     "app": "/home/anshuman-slathia/PycharmProjects/Instagram-automation/com.flipkart.android.1150200.apk",
-#     # Specify device and os_version for testing
-#     "platformVersion": "9.0",
-#     "deviceName": "Google Pixel 3",
-# })
 
 desired_cap = {
     # Set URL of the application under test
-    "app": os.getcwd() + "/com.facebook.katana_0_apps.evozi.com.apk",
-    "platformVersion": "12.0",
+    "app": os.getcwd() + "/com.flipkart.android.1150200.apk",
+    "platformVersion": "13",
     "platformName": "Android",
-    "deviceName": "Pixel 4a",
+    "deviceName": "Pixel 6 Pro",
     "appPackage": "",
-    "appWaitActivity": ""
+    "appWaitActivity": "",
+    'newCommandTimeout': 20000,
+    'deviceReadyTimeout': 2000
 }
 driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_cap)
 driver.implicitly_wait(30)
-
+# import pdb; pdb.set_trace()
 # If you have uploaded your app, update the test case here.
-# search_element = WebDriverWait(driver, 30).until(
-#     EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia"))
-# )
-# search_element.click()
-# search_input = WebDriverWait(driver, 30).until(
-#     EC.element_to_be_clickable(
-#         (AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text"))
-# )
-# search_input.send_keys("BrowserStack")
-# time.sleep(5)
-# search_results = driver.find_elements(AppiumBy.CLASS_NAME, "android.widget.TextView")
-# assert (len(search_results) > 0)
-# # Invoke driver.quit() after the test is done to indicate that the test is completed.
-# driver.quit()
+select_language = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((AppiumBy.XPATH, "//android.widget.TextView[@text='English']")))
+
+select_language.click()
+continue_button = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable(
+        (AppiumBy.XPATH, "//android.widget.Button[@text='CONTINUE']"))
+)
+time.sleep(5)
+search_results = driver.find_elements(AppiumBy.CLASS_NAME, "android.widget.TextView")
+assert (len(search_results) > 0)
+# Invoke driver.quit() after the test is done to indicate that the test is completed.
+driver.quit()
